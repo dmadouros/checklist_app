dfe792c12f0d24cbb1a93c781717e97c93e3bf47
David Madouros <dmadouros@gospotcheck.com>
2018-08-21 23:23:30 -0600

    Housekeeping

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,7 +1,7 @@
 class State < ApplicationRecord
   STATE_STARTED = 'Started'
 
-  scope :started, -> { find_by(name: STATE_STARTED) }
-
   validates :name, presence: true
+
+  scope :started, -> { find_by(name: STATE_STARTED) }
 end
